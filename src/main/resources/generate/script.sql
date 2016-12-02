6f6dd76fd245e302346eb1d30dabb83b94319513
zzhzzh204553 <zhouzhenhua1984@163.com>
2016-12-02 09:38:42 +0800

    修改

--- src/main/resources/generate/script.sql
+++ src/main/resources/generate/script.sql
@@ -1,17 +1,12 @@
 /*==============================================================*/
-/* Table: T_URM_HELLO                                           */
+/* Table: T_STAFF                                               */
 /*==============================================================*/
-DROP TABLE IF EXISTS T_URM_HELLO;
-create table T_URM_HELLO  ( 
-   URM_ID                   VARCHAR(20)      NOT NULL  comment '客户编号',
-   AGR_ID                   VARCHAR(32)      comment '协议ID',
-   EFF_DT                   VARCHAR(8)       comment '协议签订日期',
-   EXP_DT                   VARCHAR(8)       comment '协议到期日期',
-   AGR_RMK                  VARCHAR(60)      comment '协议备注',
-   AGR_TYP                  VARCHAR(2)       comment '协议类型',
-   AGR_FILE_ID              VARCHAR(16)      comment '协议文件ID',
-   PRIMARY KEY (URM_ID)
+DROP TABLE IF EXISTS T_STAFF;
+create table T_STAFF  ( 
+   STAFF_ID                 VARCHAR(32)      NOT NULL  comment 'UUID',
+   STAFF_NAME               VARCHAR(16)      comment '员工姓名',
+   PRIMARY KEY (STAFF_ID)
 );
 
-alter table T_URM_HELLO comment '测试表';
+alter table T_STAFF comment '员工表';
 
