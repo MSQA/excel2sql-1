504cf6cb2f37f04c7943ca16afd0d0d74e821393
zzhzzh204553 <zhouzhenhua1984@163.com>
2016-12-17 14:00:22 +0800

    执行参数

---
+++ src/main/java/com/zhouzh3/excel2sql/model/Context.java
@@ -0,0 +1,59 @@
+package com.zhouzh3.excel2sql.model;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Context {
+	private File excelFile;
+	private String[] sheetNames;
+	private boolean scriptSave;
+	private String scriptFile;
+	private boolean executeSql;
+
+	public File getExcelFile() {
+		return excelFile;
+	}
+
+	public void setExcelFile(File excelFile) {
+		this.excelFile = excelFile;
+	}
+
+	public String[] getSheetNames() {
+		return sheetNames;
+	}
+
+	public void setSheetNames(String[] sheetNames) {
+		this.sheetNames = sheetNames;
+	}
+
+	public boolean isScriptSave() {
+		return scriptSave;
+	}
+
+	public void setScriptSave(boolean scriptSave) {
+		this.scriptSave = scriptSave;
+	}
+
+	public String getScriptFile() {
+		return scriptFile;
+	}
+
+	public void setScriptFile(String scriptFile) {
+		this.scriptFile = scriptFile;
+	}
+
+	public boolean isExecuteSql() {
+		return executeSql;
+	}
+
+	public void setExecuteSql(boolean executeSql) {
+		this.executeSql = executeSql;
+	}
+
+	@Override
+	public String toString() {
+		return "Context [excelFile=" + excelFile + ", sheetNames=" + Arrays.toString(sheetNames) + ", saveFile="
+				+ scriptSave + ", scriptFileName=" + scriptFile + ", isExecute=" + executeSql + "]";
+	}
+
+}
