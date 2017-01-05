f2ab8ebfeb05d4e64c766074f8fd9da35d50d195
zzhzzh204553 <zhouzhenhua1984@163.com>
2017-01-05 14:06:00 +0800

    啥也没改

--- src/main/java/com/zhouzh3/excel2sql/util/ScriptRunnerUtil.java
+++ src/main/java/com/zhouzh3/excel2sql/util/ScriptRunnerUtil.java
@@ -1,13 +1,9 @@
 package com.zhouzh3.excel2sql.util;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
